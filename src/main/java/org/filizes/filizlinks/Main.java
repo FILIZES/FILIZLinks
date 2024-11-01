14f8c3859587e59654e2208f684756565425bc7b
FILIZES <vmamaev295@gmail.com>
2024-11-01 16:30:28 +0300

    update

--- src/main/java/org/filizes/filizlinks/Main.java
+++ src/main/java/org/filizes/filizlinks/Main.java
@@ -1,17 +1,17 @@
 package org.filizes.filizlinks;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.filizes.filizlinks.commands.LinksCommand;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.saveDefaultConfig();
+        this.getCommand("links").setExecutor(new LinksCommand(this));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
